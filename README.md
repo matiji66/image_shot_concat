8f56a45827882b1203e5ca227b04fa0a718642a1
chunfengyin <chunfengyin@pateo.com.cn>
2018-05-14 11:37:05 +0800

    add some desc

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-# image_shot_concat
+# image_shot_concat : concat the snapshot image through python!
 
-concat the snapshot image through python!
+本项目采用 python完成, 引入numpy 进行矩阵计算,以及opencv 模块加载图片, 
 
+# 图片拼接,完全采用 python实现,没有引入第三方库,并具有一定的泛化能力
+ 
+# result
+result3D 保存的是一些中间的合并结果
+最终的合并结果为merged_final1.jpg
+
+# warn
+此项目可以运行在,python环境中,针对Android或者是ios需要进行一定的转化.
+
+因为涉及图片尺寸,关键位置等影响因素,未必适合其他的截图拼接.
+
+# TODO 
+采用获取特征的方式,对比重合部分,进行拼接
+
+比如采用 opencv 的无缝融合
--- image_concateV3.py
+++ image_concateV3.py
@@ -1,59 +1,39 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # @author:Spring
+"""
+将手机截图保存的图片进行融合,拼接成一个长图
+"""
+# TODO optimize 当检查到底部的图片和下一张图片（局部相似，或者是距离在误差之内即可）相似之后，从下一界面，将对应的图像从头开始取出，覆盖上一张的底部部分图片
+# TODO 将对应的 top和bottom补全
+# TODO 尝试去除 cv2
 
 import os
-from PIL import Image
 import numpy as np
 import time
 import cv2
 
-"""
-将截图保存的图片进行融合拼接成一个长图
-
-"""
-# TODO optimize 当检查到底部的图片和下一张图片（局部相似，或者是距离在误差之内即可）相似之后，
-# TODO 从下一界面，将对应的图像从头开始取出，覆盖上一张的底部部分图片
-
-# 图片压缩后的大小
-width_i = 200
-height_i = 300
-
-# 每行每列显示图片数量
-line_max = 10
-row_max = 10
-
 # 参数初始化
-all_path = []
-num = 0
-pic_max = line_max * row_max
-
-dirName = os.getcwd()
-
+# dirName = os.getcwd()
 images_dir = "./images"
-
 all_path = [images_dir + "/" + file for file in os.listdir(images_dir) if file.endswith("png") or file.endswith("jpg")]
 
 print(all_path)
+num = len(all_path)
 
 
 def get_image_height(path):
-    image = Image.open(path)
-    return image.size
+    image = cv2.imread(path)
+    return image.shape
 
 
-WIDTH, HEIGHT = get_image_height(all_path[0])
+HEIGHT, WIDTH, CHANNEL = get_image_height(all_path[0])
 print(HEIGHT)
 
 TOP_PADDING = 106
 BOTTOM_Y = 597
 BOTTOM_PADDING = HEIGHT - BOTTOM_Y
 SUB_IMAGE_HEIGHT = BOTTOM_Y - TOP_PADDING
-
-Y = 0  # BOTTOM_Y
-
-toImage = Image.new('RGB', (WIDTH, HEIGHT))
-num = len(all_path)
 
 
 def concat_by_channel(image1, image2):
@@ -67,7 +47,6 @@ def distance2D(bbx1, bbx2):
     mean = np.mean(bbx1)
     sqr = np.square(bbx1 - bbx2)
     sum = np.sum(sqr)
-
     bb = np.sqrt(sum) / mean
     # print(" bb is ", bb)
     return bb
@@ -97,6 +76,15 @@ def concate_image(img1, img2, from_y, to_y, overwrite=False, compare_H=36):
 
     """
     img = []
+    # final = cv2.hconcat((img1, img2))
+    # cv2.imshow('final', final)
+    # 纵向拼接
+    # im = cv2.vconcat(src=(img1, img2), dst=img1)
+    # cv2.imshow("mergedimage", img1)
+    # image = np.concatenate((gray1, gray2))
+    # 纵向连接=np.vstack((gray1, gray2))
+    # 横向连接image = np.concatenate([gray1, gray2], axis=1)
+
     if from_y == 0 and overwrite:
         print("overwrite ...")
         img1[img1.shape[0] - (to_y - from_y):, :, :] = img2[0:, :, :]
@@ -113,15 +101,6 @@ def concate_image(img1, img2, from_y, to_y, overwrite=False, compare_H=36):
     else:
         print("error invalid from_y({}) >= to_y({})".format(from_y, to_y))
         raise AssertionError(from_y >= to_y)
-
-    # final = cv2.hconcat((img1, img2))
-    # cv2.imshow('final', final)
-    # 纵向拼接
-    # im = cv2.vconcat(src=(img1, img2), dst=img1)
-    # cv2.imshow("mergedimage", img1)
-    # image = np.concatenate((gray1, gray2))
-    # 纵向连接=np.vstack((gray1, gray2))
-    # 横向连接image = np.concatenate([gray1, gray2], axis=1)
     return img
 
 
@@ -130,19 +109,16 @@ def concate_or_replace_image(image1, image2):
     遍历y 方向，并在x方向采样，取出几个关键区域进行比对，得到的距离和
     如果说是距离为0，直接跳出循环，得到对应y
 
-    方案1
-    否则继续进行得到所有的y方向的距离索引 WARN 此处可能因为相似对很高导致的图片距离小于一定的距离机会误判断
-    并排序，得到距离最小的y的索引从该位置进行拼接（ 或者说是尝试直接跳过，认为是不重叠的直接进行拼接，因为图像不变性）
+    距离不为0 方案:
+    采用左右距离比较的方式,如果左边距离在容差之内,认为是重合的部分,如果大于容差,则计算右边部分的距离,进行容差比对,
+    如果距离在容差之内,切min_index接近, 认为是重叠部分
+    否则不重叠,进行相应的全图拼接
 
     :param image1:
     :param image2:
     :return:
     """
-
     print("concate_or_replace_image ...")
-    b, g, r = cv2.split(image1)
-
-    b2, g2, r2 = cv2.split(image2)
     # 并取出底部的左边部分的局部图片 t1, t2, t3, t4
     N = 12
     # STEP = (95 - 10) // (N + 1)
@@ -158,8 +134,8 @@ def concate_or_replace_image(image1, image2):
     distances1 = []
     distances2 = []
     for y in range(y2 + 1):
-        dist = 0
-        dist2 = 0
+        dist = 0  # 左边部分的距离
+        dist2 = 0  # 右边部分的距离
         for i in range(0, N):
             im_sub1 = image1[y1:y1 + compare_H, 10 + (i + 1) * STEP:10 + (i + 1) * STEP + PAD, :]
             im_sub2 = image2[y:y + compare_H, 10 + (i + 1) * STEP:10 + (i + 1) * STEP + PAD, :]
@@ -178,9 +154,9 @@ def concate_or_replace_image(image1, image2):
 
     min_index = np.argmin(distances1)
     min_dis1 = distances1[min_index]
-
     min_index2 = np.argmin(distances2)
     min_dis2 = distances2[min_index2]
+    # 距离容差
     DISTANCES1 = 10  # 16.689446591758163  # 60.272  # 23075.png 59.272158265002744 6696.png dis is:95.90278204692277
     DISTANCES2 = 10  # 16.689446591758163  # 60.272  # 23075.png 59.272158265002744 6696.png dis is:95.90278204692277
 
@@ -192,18 +168,12 @@ def concate_or_replace_image(image1, image2):
         print("distance is zero in index ", min_index)
         if min_index < y2:
             print("1. concate image from {} to {}", min_index, image2H)
-            # TODO  fix from min to image2H (conca then replace with par2)
             image = concate_image(image1, image2, min_index + compare_H, image2H, compare_H=compare_H)
-            # cv2.imshow('image', image)
-            # cv2.imwrite(result_images.format(time.time()), image)
         else:
             print("2. overwrite image from {} to {}".format(0, image2H))
             image = concate_image(image1, image2, 0, image2H, overwrite=True)
-            # cv2.imwrite(result_images.format(time.time()), image)
-            # cv2.imshow('image', image)
     elif len(distances1) == y2 + 1 and min_dis1 < DISTANCES1: # (distances2[min_index] < DISTANCES2) and ( min_dis1 < DISTANCES1 or abs(min_index2-min_index) < 5):  # 遍历到最后，发现距离很小，认为是重合的，
         image = concate_image(image1, image2, min_index + compare_H, image2H, overwrite=False)
-        # cv2.imwrite(result_images.format(time.time()), image)
         print("3. iterate to the end y:", y2, " and find min distance ")
 
     elif len(distances1) == y2 + 1 and min_dis1 >= DISTANCES1 :  # 遍历到最后发现，没有任何重合的，则进行拼接操作
@@ -211,40 +181,35 @@ def concate_or_replace_image(image1, image2):
             image = concate_image(image1, image2, min_index+compare_H, image2H, overwrite=False)
         else:
             image = concate_image(image1, image2, 0, image2H, overwrite=False)
-        # cv2.imwrite(result_images.format(time.time()), image)
         print("4. concate the imges from {} to {}", 0, image2H)
     else:
-        print(" other case !!! ")
+        print(" other case return image1 !!! ")
+        image = image1
     print("y_index is ===== ", min_index, min_dis1)
-    # time.strptime(a, "%Y-%m-%d %H:%M:%S")
     return image
     # 将 ti 进行 与b2的纵向遍历，横向位置比对，计算出距离 ，并求和
 
 
 if __name__ == '__main__':
-    result_images = "./result/meged{}.jpg"
-    result_images = "./result3D/meged{}.jpg"
-
+    # result_images = "./result/merged{}.jpg"
+    result_images = "./result3D/merged{}.jpg"
     image_conc = []
     image_b = []
     for i in range(0, num):
         print("process image {} ".format(all_path[i]))
         img = cv2.imread(all_path[i])
         cut_img = img[TOP_PADDING:TOP_PADDING + SUB_IMAGE_HEIGHT, 0:WIDTH]
-        # b, g, r = cv2.split(cut_img)
         if i == 0:
             print("===== init first image ======")
             image_conc = cut_img
             # image_b = b
         else:
             # 对下面的一张图片进行局部比对
-            if '1524553605795' in all_path[i]:
-                print(" process  1524553605795")
-
+            # if '1524553605795' in all_path[i]:
+            #     print(" process  1524553605795")
             image_conc = concate_or_replace_image(image_conc, cut_img)
-            cv2.imwrite(result_images.format(time.time()), image_conc)
+            cv2.imwrite(result_images.format(time.time()), image_conc)  # write tmp image to file for debug
             print()
-
-    cv2.imwrite(result_images.format(time.time()), image_conc)
-
-    # break
+            # break  # for test
+    # save concatenated image
+    cv2.imwrite("merged_final1.jpg", image_conc)
